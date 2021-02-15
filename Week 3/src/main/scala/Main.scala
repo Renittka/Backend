1fb876ccea419d00e667f407959216092dec18fd
Anna Ogorodova <renita5701@gmail.com>
2021-02-15 15:05:35 +0600

    update

--- Week 3/src/main/scala/Main.scala
+++ Week 3/src/main/scala/Main.scala
@@ -260,17 +260,63 @@ object Main extends App {
 
     trait Actor
 
-    class Speak extends Actor {
-      def receive = {
-        case StartSpeakingMessage(textToSpeak) =>
-        // code to speak the text
-        case StopSpeakingMessage =>
-        // code to stop speaking
-        case PauseSpeakingMessage =>
-        // code to pause speaking
-        case ResumeSpeakingMessage =>
-        // code to resume speaking
-      }
-    }
+//    class Speak extends Actor {
+//      def receive(): Any = {
+//        case StartSpeakingMessage("textToSpeak") =>
+//        // code to speak the text
+//        case StopSpeakingMessage =>
+//        // code to stop speaking
+//        case PauseSpeakingMessage =>
+//        // code to pause speaking
+//        case ResumeSpeakingMessage =>
+//        // code to resume speaking
+//      }
+//    }
   }
+
+  def f6(): Unit ={
+    val ints = ArrayBuffer[Int]()
+    val names = ArrayBuffer[String]()
+    ints += 1
+    ints += 2
+
+    // add one element
+    ints += 4
+
+    // add multiple elements
+    ints += 5 += 6
+
+    // add multiple elements from another collection
+    ints ++= List(7, 8, 9)
+    println(ints)
+    // remove one element
+    ints -= 9
+
+    // remove multiple elements
+    ints -= 7 -= 8
+
+    // remove multiple elements using another collection
+    ints --= Array(5, 6)
+    val a = ArrayBuffer(1, 2, 3)
+    a.append(4)                          // ArrayBuffer(1, 2, 3, 4)
+    a.append(5, 6)                       // ArrayBuffer(1, 2, 3, 4, 5, 6)
+    a.appendAll(Seq(7,8))                // ArrayBuffer(1, 2, 3, 4, 5, 6, 7, 8)
+    a.clear                              // ArrayBuffer()
+
+    val b = ArrayBuffer(9, 10)           // ArrayBuffer(9, 10)
+    b.insert(0, 8)                       // ArrayBuffer(8, 9, 10)
+    b.insertAll(0, Vector(4, 5, 6, 7))   // ArrayBuffer(4, 5, 6, 7, 8, 9, 10)
+    b.prepend(3)                         // ArrayBuffer(3, 4, 5, 6, 7, 8, 9, 10)
+    b.prepend(1, 2)                      // ArrayBuffer(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    b.prependAll(Array(0))               // ArrayBuffer(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    val c = ArrayBuffer.range('a', 'h')  // ArrayBuffer(a, b, c, d, e, f, g)
+    c.remove(0)                          // ArrayBuffer(b, c, d, e, f, g)
+    c.remove(2, 3)                       // ArrayBuffer(b, c, g)
+
+    val d = ArrayBuffer.range('a', 'h')  // ArrayBuffer(a, b, c, d, e, f, g)
+    d.trimStart(2)                       // ArrayBuffer(c, d, e, f, g)
+    d.trimEnd(2)                         // ArrayBuffer(c, d, e)
+  }
+  f6()
 }
