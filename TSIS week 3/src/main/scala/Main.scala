12d88b380cc610c5ba9a2bb2aa73f712609f2463
Anna Ogorodova <renita5701@gmail.com>
2021-02-16 17:10:41 +0600

    update

--- TSIS week 3/src/main/scala/Main.scala
+++ TSIS week 3/src/main/scala/Main.scala
@@ -1,21 +1,30 @@
-object Solution extends App{
-  def kidsWithCandies(candies: Array[Int], extraCandies: Int) = {
-    var result = new Array[Boolean](candies.length)
-    for ( candy <- candies) {
-      if (candy + extraCandies >= candies.max) {
-        result = result :+ true
-      }
-      else
-        result = result :+ false
-//      println(extraCandies + candies.max)
-//      println(candy)
-//      println(candies.max)
-    }
+import java.util
 
-   for (res <- result) println(res)
+object Solution extends App{
+  def kidsWithCandies(candies: Array[Int], extraCandies: Int): Array[Boolean] = {
+    var result = Array.emptyBooleanArray
+    for ( candy <- candies) {
+      if ((candy + extraCandies) >= candies.max) result = result.:+(true)
+      else result = result.:+(false)
+    }
+    result
+
+//    val max = candies.max
+//      candies.map { candy =>
+//        candy + extraCandies >= max
+//    }
+  }
+  //Definition for singly-linked list.
+  class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+  }
+
+  def getDecimalValue(head: ListNode): Int = {
+    while (head.next != null) {
   }
   var candies =  Array[Int](2,3,5,1,3)
-//  println(kidsWithCandies(candies, extraCandies = 3))
   kidsWithCandies(candies, extraCandies = 3)
+
+//  getDecimalValue(1)
 }
-// : Array[Boolean]
