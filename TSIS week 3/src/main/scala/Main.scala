2b120de56efb0e13d07773d9b48cadaa43e4bdbe
Anna Ogorodova <renita5701@gmail.com>
2021-02-17 17:23:24 +0600

    update

--- TSIS week 3/src/main/scala/Main.scala
+++ TSIS week 3/src/main/scala/Main.scala
@@ -1,6 +1,3 @@
-
-import java.util
-
 object Solution extends App{
   def kidsWithCandies(candies: Array[Int], extraCandies: Int): Array[Boolean] = {
     var result = Array.emptyBooleanArray
@@ -23,46 +20,47 @@ object Solution extends App{
   }
 
   def getDecimalValue(head: ListNode): Int = {
-    var count = 0
-    var h = head
-    def rec(head: ListNode): Unit ={
+    def rec(next: ListNode, current: Int): Int ={
+      val result = (current << 1) + next.x
+      if (next.next == null) result
+      else rec(next.next, result)
+    }
+
+    rec(head, 0)
 
     }
-    while (head.next != null) {
-      count += 1
-      h = h.next
-    }
-  }
 
   def smallerNumbersThanCurrent(nums: Array[Int]): Array[Int] = {
-    var result = Array(0, 1, 2)
-    result
+    val sorted = nums.sorted
+    nums.map(n => sorted.count(_ < n))
   }
 
   def repeatedNTimes(A: Array[Int]): Int = {
-    var a = 0
-    a
+    val sort = A.sorted
+    var c = 0
+    for (s <- sort){
+
+    }
+    c
   }
 
+  // Task 1
   var candies =  Array[Int](2,3,5,1,3)
-  kidsWithCandies(candies, extraCandies = 3)
+//  kidsWithCandies(candies, extraCandies = 3)
 
+  // Task 2
   var h1 = new ListNode(1)
   var h2 = new ListNode(0, h1)
-  var h3 = new ListNode(0, h2)
-  var h4 = new ListNode(1, h3)
-  var h5 = new ListNode(0, h4)
-  var h6 = new ListNode(0, h5)
-  var h7 = new ListNode(1, h6)
-  var h8 = new ListNode(1, h7)
-  var h9 = new ListNode(1, h8)
-  var h10 = new ListNode(0, h9)
-  var h11 = new ListNode(0, h10)
-  var h12 = new ListNode(0, h11)
-  var h13 = new ListNode(0, h12)
-  var h14 = new ListNode(0, h13)
-  var h15 = new ListNode(0, h14)
+  var h3 = new ListNode(1, h2)
+//  println(getDecimalValue(h3))
 
-  //1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
-  getDecimalValue(h15)
+  // Task 3
+  var input = Array(8,1,2,2,3)
+  for ( i <- smallerNumbersThanCurrent(input)){
+    println(i)
+  }
+
+  // Task 4
+  var a = Array(1,2,3,3)
+  println(repeatedNTimes(a))
 }
